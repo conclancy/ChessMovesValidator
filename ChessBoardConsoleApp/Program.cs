210074e0a1d4da57d8322d8741df7283806bdd0f
conclancy <conclancy@hotmail.com>
2020-11-14 12:18:20 -0500

    initial commit w/ only rook movement

---
+++ ChessBoardConsoleApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using ChessBoardModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoardConsoleApp
+{
+    class Program
+    {
+        static Board myBoard = new Board(8);
+
+        static void Main(string[] args)
+        {
+            // show an empty board
+            printBoard(myBoard);
+
+            while (true)
+            {
+                // prompt for an x and y coordinate
+                Cell currentCell = setCurrentCell();
+                currentCell.CurrentlyOccupied = true;
+
+                // calculate all legal moves for the piece 
+                myBoard.MarkNextLegalMoves(currentCell, "Knight");
+
+                // print new board.  Use X for occupied cell, + for legal move, _ for empty cell
+                printBoard(myBoard);
+
+                // Ask player if they want to play again
+                Console.WriteLine("Play Again? [Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+                    continue;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+        }
+
+        private static Cell setCurrentCell()
+        {
+            // ask user for x and y coordinate; return cell location
+            Console.WriteLine("Enter the current row number");
+            int currentRow = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the current column number");
+            int currentColumn = int.Parse(Console.ReadLine());
+
+            myBoard.theGrid[currentRow, currentColumn].CurrentlyOccupied = true;
+            return myBoard.theGrid[currentRow, currentColumn];
+        }
+
+        private static void printBoard(Board myBoard)
+        {
+            // print the chess board to the console.  Use X for occupied cell, + for legal move, _ for empty cell 
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                Console.WriteLine("+---+---+---+---+---+---+---+---+");
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    Cell c = myBoard.theGrid[i, j];
+
+                    if (c.CurrentlyOccupied == true)
+                    {
+                        Console.Write("| X ");
+                    }
+                    else if (c.LegalNextMove == true)
+                    {
+                        Console.Write("| + ");
+                    }
+                    else
+                    {
+                        Console.Write("|   ");
+                    }
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            Console.WriteLine("+---+---+---+---+---+---+---+---+");
+            // Console.WriteLine("============");
+        }
+    }
+}
---
+++ ClassLibrary1/Board.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoardModel
+{
+    public class Board
+    {
+        // standard board is 8x8
+        public int Size { get; set; }
+        public Cell[,] theGrid { get; set; }
+
+        // constructor for board
+        public Board(int s)
+        {
+            // initial size of board defined by s
+            Size = s;
+
+            // create a new 2D array of cells
+            theGrid = new Cell[Size, Size];
+
+            // fill the 2D array with new Cells, each with unique x and y coordinates 
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+                }
+            }
+        }
+
+        public void MarkNextLegalMoves(Cell currentCell, string chessPiece)
+        {
+            // step 1 - clear all previous legal moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j].LegalNextMove = false;
+                    theGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+            // step 2 - find all legal moves and make the cells "legal"
+            switch (chessPiece)
+            {
+                case "Knight":
+                    if (IsSafe(currentCell.RowNumber + 2, currentCell.ColumnNumber + 1))
+                    {
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    }
+
+                    if (IsSafe(currentCell.RowNumber + 2, currentCell.ColumnNumber - 1))
+                    {
+                        theGrid[currentCell.RowNumber + 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+
+                    if (IsSafe(currentCell.RowNumber - 2, currentCell.ColumnNumber + 1))
+                    {
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber + 1].LegalNextMove = true;
+                    }
+
+                    if (IsSafe(currentCell.RowNumber - 2, currentCell.ColumnNumber - 1))
+                    {
+                        theGrid[currentCell.RowNumber - 2, currentCell.ColumnNumber - 1].LegalNextMove = true;
+                    }
+
+                    if (IsSafe(currentCell.RowNumber + 1, currentCell.ColumnNumber + 2))
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    }
+
+                    if (IsSafe(currentCell.RowNumber + 1, currentCell.ColumnNumber - 2))
+                    {
+                        theGrid[currentCell.RowNumber + 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    }
+
+                    if (IsSafe(currentCell.RowNumber - 1, currentCell.ColumnNumber + 2))
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber + 2].LegalNextMove = true;
+                    }
+
+                    if (IsSafe(currentCell.RowNumber - 1, currentCell.ColumnNumber - 2))
+                    {
+                        theGrid[currentCell.RowNumber - 1, currentCell.ColumnNumber - 2].LegalNextMove = true;
+                    }
+                    break;
+
+                case "King":
+                    break;
+
+                case "Rook":
+                    break;
+
+                case "Bishop":
+                    break;
+
+                case "Queen":
+                    break;
+
+                default:
+                    break;
+            }
+            theGrid[currentCell.RowNumber, currentCell.ColumnNumber].CurrentlyOccupied = true;
+        }
+
+        private bool IsSafe(int row, int column)
+        {
+            bool rowCheck = row >= 0 && row < Size;
+            bool columnCheck = column >= 0 && column < Size;
+            return rowCheck && columnCheck;
+        }
+    }
+}
